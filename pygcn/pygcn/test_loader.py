0c46741795c5fb6679b8e67bb113823a4eabe1a6
ma8sa <you@example.com>
2018-08-04 17:54:24 +0530

    testing batch/ dataloader functionality

---
+++ pygcn/pygcn/test_loader.py
@@ -0,0 +1,119 @@
+import os
+import torch
+import numpy as np
+from torch.utils.data import Dataset , DataLoader
+import scipy.sparse as sp
+
+def encode_onehot(labels):
+    classes = [1,2,3] 
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
+                    enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)),
+                             dtype=np.int32)
+    return labels_onehot
+
+def euc_dist(x_i,y_i,x_j,y_j):
+   
+    return np.sqrt( (float(x_i-x_j))**2 + (float(y_i-y_j))**2 )
+
+def to_sparse(x):
+    """ converts dense tensor x to sparse format """
+    x_typename = torch.typename(x).split('.')[-1]
+    sparse_tensortype = getattr(torch.sparse, x_typename)
+
+    indices = torch.nonzero(x)
+    if len(indices.shape) == 0:  # if all elements are zeros
+        return sparse_tensortype(*x.shape)
+    indices = indices.t()
+    values = x[tuple(indices[i] for i in range(indices.shape[0]))]
+    return sparse_tensortype(indices, values, x.size())
+
+
+def normalize(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+def make_adj(x,y,window,sigma=50.0):
+   
+    n,_ = x.shape
+    ind = [[0,0]]
+    val = [1.0]
+    N = n * (window-1)
+    adj = np.zeros((N,N),dtype=np.float64)
+    for i in range(N):
+        for j in range(N):
+
+            frame_i = int(i / n)
+            frame_j = int(j / n)
+
+            idx_i = i % n
+            idx_j = j % n
+
+            
+	    # condition for temorapl consistency
+            if idx_i == idx_j:
+               #ind.append([i,j])
+               #val.append(1.0)
+               adj[i,j] = 1.0
+               
+            elif frame_i == frame_j:
+               # exp(-1* euc_dist(x,y,x,y)/sigma)
+               euc1 =euc_dist(x[idx_i,frame_i],y[idx_i,frame_i],x[idx_i,frame_i+1],y[idx_i,frame_i+1] )
+               euc2 =euc_dist(x[idx_j,frame_j],y[idx_j,frame_j],x[idx_j,frame_j+1],y[idx_j,frame_j+1] )
+
+               tmp = np.exp( ((-1) * ((euc1 - euc2)**2)) / sigma )
+               adj[i,j] = tmp
+               #tmp = 0.0 
+               #ind.append([i,j])
+               #val.append(tmp)
+    adj = normalize(adj)                
+    adj = torch.FloatTensor(adj)
+#    adj = to_sparse(adj)
+    return adj
+    
+    
+
+class HopkinsDataset(Dataset):
+
+
+
+      def __init__(self, window, root_dir, transform=None):
+
+          self.window = window
+          self.root_dir = root_dir
+          self.transform = transform
+
+
+      def __len__(self):
+         
+         return len(os.listdir(self.root_dir))-1
+
+
+      def __getitem__(self,idx):
+          
+          tmp = np.genfromtxt(self.root_dir + str(idx).zfill(6) + '.txt')
+          nf , _ = tmp.shape 
+          gt = tmp[:,2*self.window] 
+          tmp = tmp[:,:2*self.window] 
+          x = tmp[:,0::2]
+          y = tmp[:,1::2]
+
+          features = torch.FloatTensor(np.ones((nf,100)))
+          for _ in range(self.window-2): 
+             features = torch.cat((features,features), 0)
+             gt = np.concatenate((gt,gt),axis=0)
+          #labels = encode_onehot(gt)
+          gt = gt -1
+          labels = torch.LongTensor(gt)
+          
+          adj = make_adj(x,y,self.window)
+
+          # make adj matrix 
+          # experiment 1 features just has 1 in it
+          
+          return adj,features,labels
---
+++ pygcn/pygcn/test_train.py
@@ -0,0 +1,202 @@
+from __future__ import division
+from __future__ import print_function
+
+import time
+import argparse
+import shutil
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+from pygcn.utils import load_data, accuracy
+from test_models import GCN
+from test_loader import HopkinsDataset,to_sparse
+from torch.utils.data.sampler import SubsetRandomSampler
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False,
+                    help='Validate during training pass.')
+parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.01,
+                    help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16,
+                    help='Number of hidden units.')
+parser.add_argument('--dropout', type=float, default=0.5,
+                    help='Dropout rate (1 - keep probability).')
+
+def save_checkpoint(state, is_best, filename='test_checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'test_model_best.pth.tar')
+
+def train(epoch,loader,model,optimizer):
+    model.train()
+    optimizer.zero_grad()
+    total_loss = 0.0
+    count = 0
+    len_train = len(loader) 
+    for i ,( adj,features,labels )  in enumerate(loader):
+          # cuda()
+          features = features.cuda()
+         # adj = to_sparse(adj)
+          adj = adj.cuda()
+          labels = labels.cuda()
+          # model()
+          output = model(features, adj)
+          # loss()
+          a,b,c = output.shape
+          output = output.view(-1,c)
+          labels = labels.view(-1)
+          loss_train = F.nll_loss(output, labels)
+          
+          # optimize()
+          # update()
+          if i%40 == 0:
+             print("# {}/{} loss : {}".format(i,len_train,loss_train)) 
+          
+          loss_train.backward()
+          optimizer.step()
+          
+          count += 1
+          total_loss += loss_train
+
+
+    return (total_loss/count)
+def validate(epoch,loader,model):
+   
+    model.eval()
+    total_loss= 0.0
+    total_acc = 0.0
+    count = 0
+    val_len = len(loader)    
+    
+
+    for i in range(val_len):
+        
+          # cuda() 
+          adj,features,labels = loader[i] 
+          features = features.cuda()
+          adj = adj.cuda()
+          labels = labels.cuda()
+          # model()
+          output = model(features, adj)
+          # loss()
+          val_train = F.nll_loss(output, labels)
+          acc_val = accuracy(output, labels)
+
+          if i%40 == 0:
+             print("# {} val loss : {}, acc val:{}".format(i,val_train,acc_val)) 
+
+          count += 1
+          total_loss += val_train
+          total_acc += acc_val
+
+    return total_loss/count,total_acc/count
+
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+# Load data
+#adj, features, labels, idx_train, idx_val, idx_test = load_data()A
+loader = HopkinsDataset(window = 3 , root_dir = '../../train_dataset_03/' )
+val_loader = HopkinsDataset(window = 3 , root_dir = '../../val_dataset_03/' )
+
+dataset_len = len(loader)
+
+
+#train_sampler = SubsetRandomSampler(train_idx)
+#validation_sampler = SubsetRandomSampler(validation_idx)
+
+train_loader = torch.utils.data.DataLoader(loader, 
+                batch_size=1,num_workers=4 )
+
+#validation_loader = torch.utils.data.DataLoader(loader, 
+ #               batch_size=2, sampler=validation_sampler)
+
+
+model = GCN(nfeat=100,
+            nhid=args.hidden,
+            nclass=3,
+            dropout=args.dropout)
+
+optimizer = optim.Adam(model.parameters(),
+                       lr=args.lr, weight_decay=args.weight_decay)
+
+
+#for adj, features, labels in loader: 
+#tt = adj 
+#print(features.shape)
+#input()
+# Model and optimizer
+#  print(adj)
+ # input()
+ # model.train()
+ # optimizer.zero_grad()
+ # output = model(features, adj)
+ # loss_train = F.nll_loss(output, labels)
+ # print(loss_train)
+ # input()
+ # loss_train.backward()
+ # optimizer.step()
+ # print(" step one done")
+ # input()
+
+
+if args.cuda:
+    model.cuda()
+    #features = features.cuda()
+    #adj = adj.cuda()
+    #labels = labels.cuda()
+    #idx_train = idx_train.cuda()
+    #idx_val = idx_val.cuda()
+    #idx_test = idx_test.cuda()
+
+
+
+# Train model
+t_total = time.time()
+best_val_loss = 100000
+for epoch in range(args.epochs):
+    print(" epoch # {}".format(epoch))
+
+    train_loss = train(epoch,train_loader,model,optimizer)
+    print(" Train Loss = {}".format(train_loss))
+    val_loss, acc = validate(epoch,val_loader,model)
+    print(" val Loss = {}, accuracy : {}".format(val_loss,acc))
+
+    is_best=False
+
+    if val_loss < best_val_loss:
+       best_val_loss = val_loss
+       is_best = True
+       
+    save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_prec1': 0,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best)
+ 
+       
+    
+#print("Optimization Finished!")
+#print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+
+# Testing
+#test()
